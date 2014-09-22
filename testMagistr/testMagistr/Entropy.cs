b6d22a88c055d5e1955c76b908c4ac2fd46315d2
serbii2010 <serbii2010@gmail.com>
2014-09-23 00:26:33 +0700

    распарралелил вычисления

--- testMagistr/testMagistr/Entropy.cs
+++ testMagistr/testMagistr/Entropy.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace testMagistr
 {
@@ -16,22 +18,38 @@ namespace testMagistr
             buffer = text.Split(' ');
             List<string> words = buffer.ToList();
 
-            foreach (string word in words)
+            Mutex mutex= new Mutex();
+            Parallel.ForEach(words, word =>
             {
+                mutex.WaitOne();
                 try
                 {
-                    dictionary[word]++;
+                    dictionary[word]++;   
                 }
                 catch (Exception)
                 {
                     dictionary.Add(word, 1);
                 }
-            }
+                mutex.ReleaseMutex();
+            });
+
+            //foreach (string word in words)
+            //{
+                
+            //}
             double H = 0;
-            foreach (string word in words)
+
+            Parallel.ForEach(words, word =>
             {
+                mutex.WaitOne();
                 H += (double)dictionary[word] / words.Count * Math.Log((double)dictionary[word] / words.Count);
-            }
+                mutex.ReleaseMutex();
+            });
+
+            //foreach (string word in words)
+            //{
+            //    H += (double)dictionary[word] / words.Count * Math.Log((double)dictionary[word] / words.Count);
+            //}
             
             H *= -1;
             return (float) H;
