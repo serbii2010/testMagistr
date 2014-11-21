12713249ca66c3d15bbfcbf568fee00e6538e829
serbii2010 <serbii2010@gmail.com>
2014-11-20 23:44:23 -0200

    добавил regions

--- testMagistr/testMagistr/Form1.cs
+++ testMagistr/testMagistr/Form1.cs
@@ -20,19 +20,25 @@ namespace testMagistr
             openFileDialog1.ShowDialog();
             if (openFileDialog1.FileName != "")
             {
+                #region энтропия
                 /*
                 var H = Entropy.getEntropyChar(Separator.GetWords(openFileDialog1.FileName, checkBox1.Checked));
                 textBox1.Text = H.ToString();
                 H = Entropy.getEntropyWords(Separator.GetWords(openFileDialog1.FileName, checkBox1.Checked));
                 textBox1.Text += "\r\n"+H;
                 
+                                
                 for (var i = 1; i <= 10; i++)
                 {
                     H = Entropy.getEntropyWords(Separator.GetBlockWords(openFileDialog1.FileName,i,checkBox1.Checked));
                     textBox1.Text += "\r\n" + H;
                 }
                 */
+                #endregion
 
+
+                #region удаленность без слов и с норм словами
+                
                 double depth = Remoteness.getRemotenessChar(
                     Separator.GetWords(openFileDialog1.FileName, checkBox1.Checked)
                     );
@@ -41,6 +47,12 @@ namespace testMagistr
                     Separator.GetWords(openFileDialog1.FileName, checkBox1.Checked)
                     );
                 textBox2.Text += "\r\n" + depth;
+                 
+                 
+                #endregion
+                
+                #region если надо переставить слова
+                /*
                 if (checkBox2.Checked)
                 {
                     for (int i = 0; i < 100; i++)
@@ -51,9 +63,10 @@ namespace testMagistr
                         textBox2.Text += "\r\n" + depth;
                     }
                     
-                }
+                }*/
+                #endregion
 
-
+                #region парралельное вычисление удаленности разбитого на блоки текста
                 /*
                 var l = new Dictionary<int,double>();
                 Mutex mutex = new Mutex();
@@ -70,12 +83,16 @@ namespace testMagistr
                 {
                     textBox2.Text += "\r\n" + d.Value;
                 }*/
+                #endregion
+
+                #region вычисление удаленности разбитого на блоки текста
                 /*
                 for (var i = 1; i <= 10; i++)
                 {
                     depth = Remoteness.getRemoteness(Separator.GetBlockWords(openFileDialog1.FileName, i, checkBox1.Checked));
                     textBox2.Text += "\r\n" + depth;
                 }*/
+                #endregion
             }
         }
     }
